89e6e957b5bfe3e49331ed838f8549853901c006
SpamixOfficial <spamixproducer@gmail.com>
2024-09-13 19:10:24 +0200

    Dart base is added, will start working on api now!

---
+++ dart/lib_infomentor/example/lib_infomentor_example.dart
@@ -0,0 +1,6 @@
+import 'package:lib_infomentor/lib_infomentor.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ dart/lib_infomentor/test/lib_infomentor_test.dart
@@ -0,0 +1,16 @@
+import 'package:lib_infomentor/lib_infomentor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
